9aa2e818ef39ae6adefccab25eee6f551efc0cce
dialop <dianalop1998>
2023-12-01 20:11:43 -0500

    Add comments

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,10 +1,12 @@
+// -- Appointment Scheduler --//
+
 describe("Navigation", () => {
-    it("should visit root", () => {
-      cy.visit("/");
-    });
-  
-    it("Should navigate to Tuesday", () => {
-      cy.contains("[data-testid=day]", "Tuesday").click()
-        .should("have.class", "day-list__item--selected");
-    })
+  it("should visit root", () => {
+    cy.visit("/");
   });
+
+  it("Should navigate to Tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday").click()
+      .should("have.class", "day-list__item--selected");
+  })
+});
